5d3458eb379db80eaed5a0524976b7a4f847945f
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-03-24 22:54:35 +0100

    hw02: Reformat document

--- hw02/test.cpp
+++ hw02/test.cpp
@@ -23,19 +23,18 @@ using namespace std;
 */
 
 struct SPerson {
-	struct SName
-	{
+	struct SName {
 		string name;
 		string surname;
-		SName(const string & nm, const string & srnm) {
+		SName(const string &nm, const string &srnm) {
 			name = nm;
 			surname = srnm;
 		}
-		friend bool operator== (const SName & first, const SName & other) {
-			return (first.name == other.name) && (first.surname == other.surname); 
+		friend bool operator==(const SName &first, const SName &other) {
+			return (first.name == other.name) && (first.surname == other.surname);
 		}
-		friend bool operator< (const SName & first, const SName & other) {
-			if(first.surname != other.surname) {
+		friend bool operator<(const SName &first, const SName &other) {
+			if (first.surname != other.surname) {
 				return first.surname.compare(other.surname) < 0;
 			}
 			return first.name.compare(other.name) < 0;
@@ -44,8 +43,8 @@ struct SPerson {
 	SName fullname;
 	string email;
 	unsigned int salary;
-	SPerson(const string & nm, const string & srnm, const string & em, const unsigned int & sal)
-		: fullname(nm,srnm) {
+	SPerson(const string &nm, const string &srnm, const string &em, const unsigned int &sal)
+		: fullname(nm, srnm) {
 		email = em;
 		salary = sal;
 	}
@@ -57,17 +56,18 @@ private:
 	vector<shared_ptr<SPerson>> byEmail;
 	vector<shared_ptr<SPerson>> bySalary;
 
-	bool findName(const string &name, const string &surname, size_t & idx) const;
-	bool findEmail(const string &email, size_t & idx) const;
-	size_t findSalary(const string &email, const unsigned int & salary) const;
+	bool findName(const string &name, const string &surname, size_t &idx) const;
+	bool findEmail(const string &email, size_t &idx) const;
+	size_t findSalary(const string &email, const unsigned int &salary) const;
 
-	void addToVector(const shared_ptr<SPerson> & newPerson, vector<shared_ptr<SPerson>> & vec, bool (*comparator)(const SPerson &, const SPerson &));
+	void addToVector(const shared_ptr<SPerson> &newPerson, vector<shared_ptr<SPerson>> &vec, bool (*comparator)(const SPerson &, const SPerson &));
 
-	void setSalary(const shared_ptr<SPerson> & person, const unsigned int & salary);
+	void setSalary(const shared_ptr<SPerson> &person, const unsigned int &salary);
 
-	void getRank(const shared_ptr<SPerson> & person, int &rankMin, int &rankMax) const;
+	void getRank(const shared_ptr<SPerson> &person, int &rankMin, int &rankMax) const;
 
-	void del(const size_t & idxName, const size_t & idxEmail);
+	void del(const size_t &idxName, const size_t &idxEmail);
+
 public:
 	CPersonalAgenda(void);
 	~CPersonalAgenda(void);
@@ -113,9 +113,9 @@ public:
 ===============================================================================
 */
 
-CPersonalAgenda::CPersonalAgenda(void) { } //nothing
+CPersonalAgenda::CPersonalAgenda(void) {} // nothing
 
-CPersonalAgenda::~CPersonalAgenda(void) { //I guess I could empty the vectors
+CPersonalAgenda::~CPersonalAgenda(void) { // I guess I could empty the vectors
 	byName.clear();
 	byEmail.clear();
 	bySalary.clear();
@@ -127,19 +127,19 @@ CPersonalAgenda::~CPersonalAgenda(void) { //I guess I could empty the vectors
 ===============================================================================
 */
 
-bool cmpName(const SPerson & lhs, const SPerson & rhs) {
+bool cmpName(const SPerson &lhs, const SPerson &rhs) {
 	//(newPerson->fullname < (*iter)->fullname)
 	return (lhs.fullname < rhs.fullname);
 }
 
-bool cmpEmail(const SPerson & lhs, const SPerson & rhs) {
-	//newPerson->email.compare((*iter)->email) < 0
+bool cmpEmail(const SPerson &lhs, const SPerson &rhs) {
+	// newPerson->email.compare((*iter)->email) < 0
 	return (lhs.email.compare(rhs.email) < 0);
 }
 
-bool cmpSalaryEmail(const SPerson & lhs, const SPerson & rhs) {
+bool cmpSalaryEmail(const SPerson &lhs, const SPerson &rhs) {
 	//((newPerson->salary == (*iter)->salary) && (newPerson->email.compare((*iter)->email) < 0)) || (newPerson->salary < (*iter)->salary)
-	return ((lhs.salary == rhs.salary) && cmpEmail(lhs,rhs)) || (lhs.salary < rhs.salary);
+	return ((lhs.salary == rhs.salary) && cmpEmail(lhs, rhs)) || (lhs.salary < rhs.salary);
 }
 
 /*
@@ -148,13 +148,13 @@ bool cmpSalaryEmail(const SPerson & lhs, const SPerson & rhs) {
 ===============================================================================
 */
 
-//todo: binary search
+// todo: binary search
 
-bool CPersonalAgenda::findName(const string & name, const string & surname, size_t & idx) const {
-	//change to binary search
+bool CPersonalAgenda::findName(const string &name, const string &surname, size_t &idx) const {
+	// change to binary search
 	SPerson::SName finding = SPerson::SName(name, surname);
-	for(auto iter = byName.begin(); iter != byName.end(); iter++) {
-		if((*iter)->fullname == finding) {
+	for (auto iter = byName.begin(); iter != byName.end(); iter++) {
+		if ((*iter)->fullname == finding) {
 			idx = iter - byName.begin();
 			return true;
 		}
@@ -162,9 +162,9 @@ bool CPersonalAgenda::findName(const string & name, const string & surname, size
 	return false;
 }
 
-bool CPersonalAgenda::findEmail(const string & email, size_t & idx) const {
-	for(auto iter = byEmail.begin(); iter != byEmail.end(); iter++) {
-		if((*iter)->email == email) {
+bool CPersonalAgenda::findEmail(const string &email, size_t &idx) const {
+	for (auto iter = byEmail.begin(); iter != byEmail.end(); iter++) {
+		if ((*iter)->email == email) {
 			idx = iter - byEmail.begin();
 			return true;
 		}
@@ -173,12 +173,12 @@ bool CPersonalAgenda::findEmail(const string & email, size_t & idx) const {
 }
 
 size_t CPersonalAgenda::findSalary(const string &email, const unsigned int &salary) const {
-	for(auto iter = bySalary.begin(); iter != bySalary.end(); iter++) {
-		if((*iter)->salary == salary && (*iter)->email == email) {
+	for (auto iter = bySalary.begin(); iter != bySalary.end(); iter++) {
+		if ((*iter)->salary == salary && (*iter)->email == email) {
 			return iter - bySalary.begin();
 		}
 	}
-	throw logic_error ("CPersonalAgenda::findSalary: email " + email + " not found (even though it should be contained in the DB)");
+	throw logic_error("CPersonalAgenda::findSalary: email " + email + " not found (even though it should be contained in the DB)");
 }
 
 /*
@@ -189,10 +189,10 @@ size_t CPersonalAgenda::findSalary(const string &email, const unsigned int &sala
 
 void CPersonalAgenda::addToVector(const shared_ptr<SPerson> &newPerson, vector<shared_ptr<SPerson>> &vec, bool (*comparator)(const SPerson &, const SPerson &)) {
 	//! This is T(n) at worst!
-	//todo: something like binary search where to add?
-	for(auto iter = vec.begin(); iter != vec.end(); iter++) {
-		if(comparator((*newPerson),(**iter))) {
-			vec.insert(iter,newPerson);
+	// todo: something like binary search where to add?
+	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+		if (comparator((*newPerson), (**iter))) {
+			vec.insert(iter, newPerson);
 			return;
 		}
 	}
@@ -202,15 +202,15 @@ void CPersonalAgenda::addToVector(const shared_ptr<SPerson> &newPerson, vector<s
 bool CPersonalAgenda::add(const string &name, const string &surname, const string &email, unsigned int salary) {
 	shared_ptr<SPerson> newPerson = make_shared<SPerson>(name, surname, email, salary);
 	size_t temp;
-	if(findName(name,surname,temp)) {
+	if (findName(name, surname, temp)) {
 		return false;
 	}
-	if(findEmail(email, temp)) {
+	if (findEmail(email, temp)) {
 		return false;
 	}
-	addToVector(newPerson,byName,cmpName);
-	addToVector(newPerson,byEmail,cmpEmail);
-	addToVector(newPerson,bySalary,cmpSalaryEmail);
+	addToVector(newPerson, byName, cmpName);
+	addToVector(newPerson, byEmail, cmpEmail);
+	addToVector(newPerson, bySalary, cmpSalaryEmail);
 	return true;
 }
 
@@ -219,37 +219,37 @@ bool CPersonalAgenda::add(const string &name, const string &surname, const strin
 							Deleting methods
 ===============================================================================
 */
-void CPersonalAgenda::del(const size_t & idxName, const size_t & idxEmail) {
-	size_t idxSalary = findSalary(byName[idxName]->email,byName[idxName]->salary);
+void CPersonalAgenda::del(const size_t &idxName, const size_t &idxEmail) {
+	size_t idxSalary = findSalary(byName[idxName]->email, byName[idxName]->salary);
 
-	byName.erase(byName.begin()+idxName);
-	byEmail.erase(byEmail.begin()+idxEmail);
-	bySalary.erase(bySalary.begin()+idxSalary);
+	byName.erase(byName.begin() + idxName);
+	byEmail.erase(byEmail.begin() + idxEmail);
+	bySalary.erase(bySalary.begin() + idxSalary);
 }
 
 bool CPersonalAgenda::del(const string &name, const string &surname) {
 	size_t idxName;
-	if(!findName(name,surname,idxName)) {
+	if (!findName(name, surname, idxName)) {
 		return false;
 	}
 
 	size_t idxEmail;
-	findEmail(byName[idxName]->email,idxEmail);
+	findEmail(byName[idxName]->email, idxEmail);
 
-	del(idxName,idxEmail);
+	del(idxName, idxEmail);
 	return true;
 }
 
 bool CPersonalAgenda::del(const string &email) {
 	size_t idxEmail;
-	if(!findEmail(email,idxEmail)) {
+	if (!findEmail(email, idxEmail)) {
 		return false;
 	}
 
 	size_t idxName;
-	findName(byEmail[idxEmail]->fullname.name,byEmail[idxEmail]->fullname.surname,idxName);
+	findName(byEmail[idxEmail]->fullname.name, byEmail[idxEmail]->fullname.surname, idxName);
 
-	del(idxName,idxEmail);
+	del(idxName, idxEmail);
 	return true;
 }
 
@@ -260,36 +260,36 @@ bool CPersonalAgenda::del(const string &email) {
 */
 
 bool CPersonalAgenda::changeName(const string &email, const string &newName, const string &newSurname) {
-	//note to self: this is getting repetitive, am I doing this right?
+	// note to self: this is getting repetitive, am I doing this right?
 	size_t idxEmail, idxName;
-	//email not included or name is duplicite
-	if(!findEmail(email,idxEmail) || findName(newName,newSurname, idxName)) {
+	// email not included or name is duplicite
+	if (!findEmail(email, idxEmail) || findName(newName, newSurname, idxName)) {
 		return false;
 	}
-	
+
 	findName(byEmail[idxEmail]->fullname.name, byEmail[idxEmail]->fullname.surname, idxName);
 
-	byName.erase(byName.begin()+idxName);
+	byName.erase(byName.begin() + idxName);
 
-	byEmail[idxEmail]->fullname = SPerson::SName(newName,newSurname);
+	byEmail[idxEmail]->fullname = SPerson::SName(newName, newSurname);
 
-	addToVector(byEmail[idxEmail],byName,cmpName);
+	addToVector(byEmail[idxEmail], byName, cmpName);
 	return true;
 }
 
 bool CPersonalAgenda::changeEmail(const string &name, const string &surname, const string &newEmail) {
 	size_t idxName, idxEmail;
-	//name not included or email is duplicite
-	if(!findName(name, surname, idxName) || findEmail(newEmail, idxEmail)) {
+	// name not included or email is duplicite
+	if (!findName(name, surname, idxName) || findEmail(newEmail, idxEmail)) {
 		return false;
 	}
 
-	findEmail(byName[idxName]->email,idxEmail);
+	findEmail(byName[idxName]->email, idxEmail);
 
-	byEmail.erase(byEmail.begin()+idxEmail);
+	byEmail.erase(byEmail.begin() + idxEmail);
 
-	size_t idxSalary = findSalary(byName[idxName]->email,byName[idxName]->salary);
-	bySalary.erase(bySalary.begin()+idxSalary);
+	size_t idxSalary = findSalary(byName[idxName]->email, byName[idxName]->salary);
+	bySalary.erase(bySalary.begin() + idxSalary);
 
 	byName[idxName]->email = newEmail;
 
@@ -299,26 +299,26 @@ bool CPersonalAgenda::changeEmail(const string &name, const string &surname, con
 }
 
 void CPersonalAgenda::setSalary(const shared_ptr<SPerson> &person, const unsigned int &salary) {
-	bySalary.erase(bySalary.begin() + findSalary(person->email,person->salary));
+	bySalary.erase(bySalary.begin() + findSalary(person->email, person->salary));
 	person->salary = salary;
-	addToVector(person,bySalary,cmpSalaryEmail);
+	addToVector(person, bySalary, cmpSalaryEmail);
 }
 
 bool CPersonalAgenda::setSalary(const string &name, const string &surname, unsigned int salary) {
 	size_t idx;
-	if(!findName(name,surname,idx)) {
+	if (!findName(name, surname, idx)) {
 		return false;
 	}
-	setSalary(byName[idx],salary);
+	setSalary(byName[idx], salary);
 	return true;
 }
 
 bool CPersonalAgenda::setSalary(const string &email, unsigned int salary) {
 	size_t idx;
-	if(!findEmail(email,idx)) {
+	if (!findEmail(email, idx)) {
 		return false;
 	}
-	setSalary(byEmail[idx],salary);
+	setSalary(byEmail[idx], salary);
 	return true;
 }
 
@@ -330,7 +330,7 @@ bool CPersonalAgenda::setSalary(const string &email, unsigned int salary) {
 
 unsigned int CPersonalAgenda::getSalary(const string &name, const string &surname) const {
 	size_t idx;
-	if(!findName(name,surname,idx)) {
+	if (!findName(name, surname, idx)) {
 		return 0;
 	}
 	return byName[idx]->salary;
@@ -338,40 +338,42 @@ unsigned int CPersonalAgenda::getSalary(const string &name, const string &surnam
 
 unsigned int CPersonalAgenda::getSalary(const string &email) const {
 	size_t idx;
-	if(!findEmail(email,idx)) {
+	if (!findEmail(email, idx)) {
 		return 0;
 	}
 	return byEmail[idx]->salary;
 }
 
-void CPersonalAgenda::getRank(const shared_ptr<SPerson> & person, int &rankMin, int &rankMax) const {
-	size_t idx = findSalary(person->email,person->salary);
-	
-	//todo: binary search
-	for(rankMin = idx; rankMin != 0 && bySalary[rankMin-1]->salary == person->salary; rankMin--) { }
-	for(rankMax = idx; rankMax+1 != (int) bySalary.size() && bySalary[rankMax+1]->salary == person->salary; rankMax++) { }
+void CPersonalAgenda::getRank(const shared_ptr<SPerson> &person, int &rankMin, int &rankMax) const {
+	size_t idx = findSalary(person->email, person->salary);
+
+	// todo: binary search
+	for (rankMin = idx; rankMin != 0 && bySalary[rankMin - 1]->salary == person->salary; rankMin--) {
+	}
+	for (rankMax = idx; rankMax + 1 != (int)bySalary.size() && bySalary[rankMax + 1]->salary == person->salary; rankMax++) {
+	}
 }
 
 bool CPersonalAgenda::getRank(const string &name, const string &surname, int &rankMin, int &rankMax) const {
 	size_t idx;
-	if(!findName(name,surname,idx)) {
+	if (!findName(name, surname, idx)) {
 		return false;
 	}
-	getRank(byName[idx],rankMin,rankMax);
+	getRank(byName[idx], rankMin, rankMax);
 	return true;
 }
 
 bool CPersonalAgenda::getRank(const string &email, int &rankMin, int &rankMax) const {
 	size_t idx;
-	if(!findEmail(email,idx)) {
+	if (!findEmail(email, idx)) {
 		return false;
 	}
-	getRank(byEmail[idx],rankMin,rankMax);
+	getRank(byEmail[idx], rankMin, rankMax);
 	return true;
 }
 
 bool CPersonalAgenda::getFirst(string &outName, string &outSurname) const {
-	if(byName.empty()) {
+	if (byName.empty()) {
 		return false;
 	}
 	outName = byName[0]->fullname.name;
@@ -381,7 +383,7 @@ bool CPersonalAgenda::getFirst(string &outName, string &outSurname) const {
 
 bool CPersonalAgenda::getNext(const string &name, const string &surname, string &outName, string &outSurname) const {
 	size_t idx;
-	if(!findName(name,surname,idx) || byName.size() == ++idx) {
+	if (!findName(name, surname, idx) || byName.size() == ++idx) {
 		return false;
 	}
 	outName = byName[idx]->fullname.name;
