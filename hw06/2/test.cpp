a09a21dde1e875f6100f98793227f22a35bf6e6f
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-04-30 22:52:36 +0200

    hw06_2: Went through and commented and cleaned up

--- hw06/2/test.cpp
+++ hw06/2/test.cpp
@@ -46,20 +46,16 @@ public:
 #endif /* __PROGTEST__ */
 
 /// @brief Returns a new CRect with calculated absolute position
-/// @param winPos The position of a window
-/// @param relPos The relative position of an element
 /// @return The absolute position of an element
-CRect getAbsolutePos(const CRect &winPos, const CRect &relPos) {
+CRect getAbsolutePos(const CRect &parentAbsPos, const CRect &relPos) {
 	return CRect(
-		(winPos.m_W * relPos.m_X) + winPos.m_X,
-		(winPos.m_H * relPos.m_Y) + winPos.m_Y,
-		winPos.m_W * relPos.m_W,
-		winPos.m_H * relPos.m_H);
+		(parentAbsPos.m_W * relPos.m_X) + parentAbsPos.m_X,
+		(parentAbsPos.m_H * relPos.m_Y) + parentAbsPos.m_Y,
+		parentAbsPos.m_W * relPos.m_W,
+		parentAbsPos.m_H * relPos.m_H);
 }
 
 /// @brief Checks if a sorted vector of ints contains a given value
-/// @param checking Value to find
-/// @param toCheck Vector to check
 /// @return True if value was found, otherwise false
 bool isContained(const int &checking, const vector<int> &toCheck) {
 	return binary_search(toCheck.begin(), toCheck.end(), checking);
@@ -74,6 +70,11 @@ protected:
 	CRect m_absPos;
 
 	// common methods
+
+	/// @brief Prints the indentation
+	/// @param indent How much to indent
+	/// @param pipePos Position of the pipe characters
+	/// @param printLastSpace Whether to print the last space (used for CComboBox items)
 	void printIndent(ostream &os, const int &indent, vector<int> &pipePos, const bool &printLastSpace = true) const {
 		for (int i = 0; i < indent; i++) {
 			if (i == indent - 1) {
@@ -87,8 +88,10 @@ protected:
 			os << "   ";
 		}
 	}
+
 	// methods to override
 
+	/// @brief Prints the most common line, containing the id, element name, absolute position, has an override with a title
 	void printCommon(ostream &os, const int &indent, vector<int> &pipePos) const {
 		printIndent(os, indent, pipePos);
 		os << "[" << m_id << "] " << getElementName() << " " << m_absPos << "\n";
@@ -97,6 +100,7 @@ protected:
 		printIndent(os, indent, pipePos);
 		os << "[" << m_id << "] " << getElementName() << " \"" << title << "\" " << m_absPos << "\n";
 	}
+
 	virtual string getElementName() const = 0;
 
 public:
@@ -110,19 +114,23 @@ public:
 		return m_id;
 	}
 
+	/// @brief Updates the position calculated from the relative position of this window relative to the absolute position of the parent container
 	virtual void updatePosition(const CRect &parentAbsPos) {
 		m_absPos = getAbsolutePos(parentAbsPos, m_relPos);
 	}
 
+	/// @brief Searches for elements, overriden in containers
+	/// @param id The desired id to find
+	/// @return pointer to this element, when ids match, otherwise nullptr
 	virtual CElement *search(const int &id) {
 		return (m_id == id) ? this : nullptr;
 	}
 };
 
+/// @brief A common element, which contains a string
 class CTitleable : public CElement {
-private:
 protected:
-	string m_title;
+	string m_title; // Some elements (CInput) implement a getter and setter
 
 public:
 	CTitleable(const int &id, const CRect &relPos, const string &title) : CElement(id, relPos), m_title(title) {}
@@ -151,13 +159,17 @@ protected:
 		return false;
 	}
 
+	/// @brief an overriden method for containers, updates the position for children too (if the element is a window, doesn't do anything)
 	virtual void updatePosition(const CRect &parentAbsPos) override {
+		// My idea is that windows can contain windows, sort of like pop-up windows, although I don't expect ProgTest to test this
+		// I could theoretically reuse this code for the semestral project
 		if (!isCWindow()) {
 			m_absPos = getAbsolutePos(parentAbsPos, m_relPos);
+			updateChildrenPos();
 		}
-		updateChildrenPos();
 	}
 
+	/// @brief Common method for containers, which adds an element, but is not able to chain
 	void addElement(const CElement &toAdd) {
 		shared_ptr<CElement> addSptr = toAdd.clone();
 		weak_ptr<CElement> addWptr = addSptr;
@@ -168,6 +180,7 @@ protected:
 		m_elements.push_back(addSptr);
 	}
 
+	/// @brief Method for containers to print their subelements
 	void printChildren(ostream &os, int indent, vector<int> &pipePos) const {
 		if (m_elements.size() > 1) {
 			pipePos.push_back(indent);
@@ -175,10 +188,19 @@ protected:
 		++indent;
 
 		for (auto iter = m_elements.begin(); iter != m_elements.end(); ++iter) {
-			if (next(iter) == m_elements.end() && m_elements.size() > 1) { // Element is second last
+			if (next(iter) == m_elements.end() && m_elements.size() > 1) { // Element is second last = get rid of the pipe (if it was pushed back)
 				pipePos.pop_back();
 			}
 			(*iter)->print(os, indent, pipePos);
+		}
+	}
+
+	/// @brief Copies elements from another container
+	void copyElements(const vector<shared_ptr<CElement>> &copyFrom) {
+		m_elements.clear();
+
+		for (auto element : copyFrom) {
+			add(*element);
 		}
 	}
 
@@ -186,22 +208,19 @@ public:
 	using CElement::CElement;
 
 	CPanel(const CPanel &copyFrom) : CPanel(copyFrom.getId(), copyFrom.m_relPos) {
-		m_elements.clear();
-
-		for (auto element : copyFrom.m_elements) {
-			add(*element);
-		}
+		copyElements(copyFrom.m_elements);
 	}
 
 	virtual shared_ptr<CElement> clone() const override {
 		return make_shared<CPanel>(*this);
 	}
-	// I hope I won't have to implement a copy constructor...
+
 	virtual void print(ostream &os, int indent, vector<int> &pipePos) const override {
 		printCommon(os, indent, pipePos);
 		printChildren(os, indent, pipePos);
 	}
 
+	/// @brief Overriden method for containers, first checks if this is the element, otherwise iterates through all contained elements
 	virtual CElement *search(const int &id) override {
 		if (id == getId()) {
 			return this;
@@ -215,17 +234,16 @@ public:
 
 		return nullptr;
 	}
-	// add
+
 	CPanel &add(const CElement &toAdd) {
 		addElement(toAdd);
 		return *this;
 	}
 };
 
-// I'm not inheriting from CTitleable for I will later copy this implementation into hw06_2 that will inherit from CPanel which will not inherit from CTitleable
 class CWindow : public CPanel {
 private:
-	string m_title;
+	string m_title; // I had an idea to use multiple inheritance (from CTitleable) but Vagner said no. Too bad!
 
 	virtual string getElementName() const override {
 		return "Window";
@@ -235,31 +253,24 @@ private:
 		return true;
 	}
 
-protected:
 public:
 	CWindow(const int &id, const string &title, const CRect &absPos) : CPanel(id, absPos), m_title(title) {
 		swap(m_absPos, m_relPos);
 	}
 
 	CWindow(const CWindow &copyFrom) : CWindow(copyFrom.getId(), copyFrom.m_title, copyFrom.m_absPos) {
-		m_elements.clear();
-
-		for (auto element : copyFrom.m_elements) {
-			add(*element);
-		}
+		copyElements(copyFrom.m_elements);
 	}
 
 	virtual shared_ptr<CElement> clone() const override {
 		return make_shared<CWindow>(*this);
 	}
 
-	// add
 	CWindow &add(const CElement &toAdd) {
 		addElement(toAdd);
 		return *this;
 	}
 
-	// setPosition
 	void setPosition(const CRect &newPos) {
 		m_absPos = newPos;
 		updateChildrenPos();
@@ -298,15 +309,15 @@ public:
 		return make_shared<CInput>(*this);
 	}
 
-	// setValue
 	void setValue(const string &newVal) {
 		m_title = newVal;
 	}
-	// getValue
+
 	string getValue() const {
 		return m_title;
 	}
 };
+
 class CLabel : public CTitleable {
 private:
 	virtual string getElementName() const override {
@@ -324,7 +335,7 @@ public:
 class CComboBox : public CElement {
 private:
 	vector<string> m_items;
-	size_t m_selected;
+	size_t m_selected; // just in case there's a test with 1.8 * 10 ^ 19 items
 
 	virtual string getElementName() const override {
 		return "ComboBox";
@@ -337,7 +348,7 @@ public:
 
 	virtual void print(ostream &os, int indent, vector<int> &pipePos) const override {
 		printCommon(os, indent, pipePos);
-		indent++;
+		++indent;
 		for (size_t i = 0; i < m_items.size(); ++i) {
 			printIndent(os, indent, pipePos, false);
 			os << ((i == m_selected) ? (">" + m_items[i] + "<") : (" " + m_items[i]));
@@ -348,22 +359,22 @@ public:
 	virtual shared_ptr<CElement> clone() const override {
 		return make_shared<CComboBox>(*this);
 	}
-	// add
+
 	CComboBox &add(const string &toAdd) {
 		m_items.emplace_back(string(toAdd));
 		return *this;
 	}
-	// setSelected
+
 	void setSelected(const size_t &selected) {
 		m_selected = selected;
 	}
-	// getSelected
+
 	size_t getSelected() const {
 		return m_selected;
 	}
 };
 
-// output operators
+// output operator
 ostream &operator<<(ostream &os, const CElement &element) {
 	vector<int> pipePos;
 	element.print(os, 0, pipePos);
