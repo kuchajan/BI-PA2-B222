5d862ed577994de1b748500d4c9c8adab7d526cd
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-09 15:09:14 +0200

    Implement CMatrix constructors/destructors

--- semestral/src/CMatrix.hpp
+++ semestral/src/CMatrix.hpp
@@ -20,4 +20,26 @@ private:
 	T *m_data;
 
 public:
+	CMatrix() {
+		m_data == nullptr;
+	}
+	CMatrix(const int &width, const int &height) : m_width(width), m_height(height) {
+		if (m_width == 0 || m_height == 0) {
+			throw invalid_argument("CMatrix::CMatrix: Width and/or height of the matrix should not be 0.");
+		}
+		m_data = new T[m_width * m_height];
+	}
+	CMatrix(const CMatrix &src) {
+		delete[] m_data;
+
+		m_width = src.m_width;
+		m_height = src.m_height;
+
+		m_data = new T[m_width * m_height];
+		memcpy(m_data, src.m_data, m_width * m_height * sizeof(T));
+	}
+	~CMatrix() {
+		delete[] m_data;
+		m_data = nullptr;
+	}
 };
