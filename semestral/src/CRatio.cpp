898daaaa35d68ec27586176fc4df8dd89ef4d7d4
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-27 18:33:00 +0200

    Implement gcd and lcm

--- semestral/src/CRatio.cpp
+++ semestral/src/CRatio.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+/// @brief Finds the greatest common divisor between two numbers
+/// @return The greatest common divisor
+int gcd(int first, int second) {
+	while (first != second) {
+		if (first > second) {
+			first -= second;
+		} else {
+			second -= first;
+		}
+	}
+	return first;
+}
+
+/// @brief Finds the lowest common multiple between two numbers
+/// @return The lowest common multiple
+int lcm(int first, int second) {
+	return first * second / gcd(first, second);
+}
+
 CRatio::CRatio(int numerator, int denominator) : m_numerator(numerator), m_denominator(denominator) {}
 
 CRatio::CRatio(const CRatio &src) : m_numerator(src.m_numerator), m_denominator(src.m_denominator) {}
