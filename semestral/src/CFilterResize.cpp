931442f43d6b64f8d1e165855fd4e8eaf709bdc5
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-29 15:03:25 +0200

    Implement basic iterative average

--- semestral/src/CFilterResize.cpp
+++ semestral/src/CFilterResize.cpp
@@ -3,4 +3,18 @@
 
 using namespace std;
 
+char CFilterResize::average(const CMatrix<char> &input, int fromX, int toX, int fromY, int toY) {
+	double res = 0;
+	int i = 0;
+
+	for (int x = fromX; x < toX; ++x) {
+		for (int y = fromY; x < toY; ++y) {
+			res = res * i / (i + 1) + input.getData(x, y) / (i + 1);
+			++i;
+		}
+	}
+
+	return (char)res;
+}
+
 CFilterResize::CFilterResize(int width, int height) : m_newWidth(width), m_newHeight(height) {}
