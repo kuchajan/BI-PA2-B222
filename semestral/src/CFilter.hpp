adac2fbe0a83ba7be21dd99e7db5c91e5a3c838c
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-29 15:32:01 +0200

    Set up polymorphic clone method for CFilter

--- semestral/src/CFilter.hpp
+++ semestral/src/CFilter.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <memory>
+
 #include "CMatrix.hpp"
+
+using namespace std;
 
 class CFilter {
 private:
 protected:
 public:
 	virtual CMatrix<char> apply(const CMatrix<char> &input) const = 0;
+	virtual std::shared_ptr<CFilter> clone() const = 0;
 };
