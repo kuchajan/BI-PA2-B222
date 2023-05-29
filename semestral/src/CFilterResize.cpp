3ba973f1ea991c22b8df49cecb5b7e14763386fb
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-29 15:25:49 +0200

    Implement CFilterResize apply

--- semestral/src/CFilterResize.cpp
+++ semestral/src/CFilterResize.cpp
@@ -24,3 +24,17 @@ pair<int, int> CFilterResize::getRange(int index, int oldDimSize, int newDimSize
 }
 
 CFilterResize::CFilterResize(int width, int height) : m_newWidth(width), m_newHeight(height) {}
+
+CMatrix<char> CFilterResize::apply(const CMatrix<char> &input) const {
+	CMatrix<char> res(m_newWidth, m_newHeight);
+
+	for (int x = 0; x < m_newWidth; ++x) {
+		for (int y = 0; y < m_newHeight; ++y) {
+			pair<int, int> rangeX = getRange(x, input.getWidth(), m_newWidth);
+			pair<int, int> rangeY = getRange(y, input.getHeight(), m_newHeight);
+			res.setData(x, y, average(input, rangeX.first, rangeX.second, rangeY.first, rangeY.second));
+		}
+	}
+
+	return res;
+}
