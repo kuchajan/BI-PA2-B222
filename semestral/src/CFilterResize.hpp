b8cca6225ad372313f9fbd196feb24f96bebdf68
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-29 15:01:45 +0200

    Start implementation of CFilterResize

---
+++ semestral/src/CFilterResize.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CFilter.hpp"
+
+class CFilterResize : public CFilter {
+private:
+	int m_newWidth;
+	int m_newHeight;
+
+	char average(const CMatrix<char> &input, int fromX, int toX, int fromY, int toY);
+	pair<int, int> getRange(int index, int oldDimSize, int newDimSize) const;
+
+public:
+	CFilterResize(int width, int height);
+	virtual CMatrix<char> apply(const CMatrix<char> &input) const override;
+};
