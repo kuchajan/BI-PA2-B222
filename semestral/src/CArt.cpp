6b70b999b7b2c7f009ef15713b52c10933752dcb
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-05-30 19:33:50 +0200

    Copy implementation of getGrayScale

--- semestral/src/CArt.cpp
+++ semestral/src/CArt.cpp
@@ -1,5 +1,21 @@
 #include "CArt.hpp"
 
+CMatrix<unsigned char> CArt::getGrayScale(const CImage &image, const vector<shared_ptr<CFilter>> &filters) const {
+	CMatrix<unsigned char> grayScale(image.getWidth(), image.getHeight());
+
+	for (int x = 0; x < image.getWidth(); ++x) {
+		for (int y = 0; y < image.getHeight(); ++y) {
+			grayScale.setData(x, y, image.getGrayPixel(x, y));
+		}
+	}
+
+	for (shared_ptr<CFilter> filter : filters) {
+		grayScale = filter->apply(grayScale);
+	}
+
+	return grayScale;
+}
+
 CMatrix<char> CArt::getResult(const CMatrix<unsigned char> &grayScale, const CCharset &charset) const {
 	CMatrix<char> result(grayScale.getWidth(), grayScale.getHeight());
 	for (int i = 0; i < result.getElementCount(); ++i) {
