c92d32cbc85faec3c8740d559a43f8e13dbbb7a9
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-06-03 16:49:45 +0200

    Start implementation of handleInput

--- semestral/src/main.cpp
+++ semestral/src/main.cpp
@@ -39,21 +39,28 @@ void printHelp(const char *progName) {
 		 << endl;
 }
 
-int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		cout << "Usage: " << argv[0] << " file charset" << endl;
-		return 0;
-	}
-
-	if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF) == 0) {
-		throw logic_error("getImageData: IMG_Init fail");
-	}
-
-	CImage image(argv[1]);
-	CCharset charset(argv[2]);
+shared_ptr<COutput> handleInput(int argc, char *argv[]) {
 	vector<shared_ptr<CFilter>> filters;
-	CFilterResize tmpFilt(32, 16);
-	filters.push_back(tmpFilt.clone());
+	CCharset charset(" .:*I$VFNM");
+	vector<CArt> art;
+	shared_ptr<COutput> output = shared_ptr<COutput>(nullptr);
+
+	if (argc == 1) {
+		throw invalid_argument("Too few arguments");
+	}
+
+	for (int i = 1; i < argc; ++i) {
+		// todo: iterate through all arguments and their sub arguments
+
+		throw invalid_argument(string("Unknown argument: ") + argv[i]);
+	}
+
+	if (!output) {
+		throw invalid_argument("No outputter specified");
+	}
+	output->addArts(art);
+	return output;
+}
 
 	CArt art(image, charset, filters);
 
