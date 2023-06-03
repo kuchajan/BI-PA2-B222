363b7a1c7718136e57e5a71af734f84a0cc84fec
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-06-03 22:44:32 +0200

    Get rid of unnecessary lines

--- semestral/src/CFilter.hpp
+++ semestral/src/CFilter.hpp
@@ -7,8 +7,6 @@
 using namespace std;
 
 class CFilter {
-private:
-protected:
 public:
 	virtual CMatrix<unsigned char> apply(const CMatrix<unsigned char> &input) const = 0;
 	virtual std::shared_ptr<CFilter> clone() const = 0;
