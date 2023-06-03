9d7593d5b40050033ff0f51965e354462c4cbf65
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-06-03 16:51:44 +0200

    Implement COutput

---
+++ semestral/src/COutput.cpp
@@ -0,0 +1,5 @@
+#include "COutput.hpp"
+
+void COutput::addArts(vector<CArt> &src) {
+	swap(m_art, src);
+}
--- semestral/src/COutput.hpp
+++ semestral/src/COutput.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <iostream>
+#include <memory>
 #include <vector>
 
 #include "CArt.hpp"
 
 class COutput {
-private:
 protected:
-	vector<CArt> art;
+	vector<CArt> m_art;
 
 public:
 	virtual void output() = 0;
+	virtual shared_ptr<COutput> clone() = 0;
+	void addArts(vector<CArt> &src);
 };
