1772fdcde1fba09afbb19f9d9ca73f9ed4d18d99
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-06-04 20:32:46 +0200

    Document CCharset, pass by value

--- semestral/src/CCharset.cpp
+++ semestral/src/CCharset.cpp
@@ -8,9 +8,6 @@
 
 using namespace std;
 
-/// @brief Creates a new charset to use to generate art
-/// @param charset String of characters to use
-/// @exception Throws an invalid_argument when charset length is 0 or more than 256
 CCharset::CCharset(const char *charset) : m_charset(charset) {
 	size_t length = m_charset.length();
 	if (length == 0 || length > 256) {
@@ -18,6 +15,6 @@ CCharset::CCharset(const char *charset) : m_charset(charset) {
 	}
 }
 
-char CCharset::getChar(const uint8_t &value) const {
+char CCharset::getChar(uint8_t value) const {
 	return m_charset[(value * (m_charset.length())) / 256];
 }
--- semestral/src/CCharset.hpp
+++ semestral/src/CCharset.hpp
@@ -9,11 +9,20 @@
 
 using namespace std;
 
+/// @brief A character set which will be used to generate art from
 class CCharset {
 private:
+	/// @brief The charset
 	string m_charset;
 
 public:
+	/// @brief Creates a new charset to use to generate art
+	/// @param charset String of characters to use
+	/// @throws invalid_argument when charset length is 0 or more than 256
 	CCharset(const char *charset);
-	char getChar(const uint8_t &value) const;
+
+	/// @brief Gets a character from the charset by the given value
+	/// @param value The value to determine which character to return
+	/// @return The desired character
+	char getChar(uint8_t value) const;
 };
